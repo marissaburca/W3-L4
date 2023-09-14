c7fb2c3426b51baa0c4e1cef8030ece2f99047a2
Marissa Burca <marissaburca@gmail.com>
2023-09-14 18:32:40 +0200

    concluso

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
+const tabellone = [];
 
-const tabellone = []
+const numbers = function () {
+  for (let i = 1; i < 77; i++) {
+    tabellone.push(i);
+  }
 
-const numbers = function(){
-  
-    for(let i=1; i<77;i++){
-    tabellone.push(i)}
+  const createNumber = function () {
+    for (let i = 0; i < tabellone.length; i++) {
+      const grill = document.getElementById("tabellone");
+      const newGrillElement = document.createElement("span");
+      newGrillElement.classList.add("numero");
+      newGrillElement.innerText = tabellone[i];
+      grill.appendChild(newGrillElement);
 
-    const createNumber = function(){
-        for(let i=0; i < tabellone.length;i++) {
-        const grill = document.getElementById('tabellone')
-        const newGrillElement = document.createElement('span')
-        newGrillElement.classList.add('numero')
-        newGrillElement.innerText = tabellone[i]
-        grill.appendChild(newGrillElement)
-       
-        console.log(newGrillElement)
+      console.log(newGrillElement);
     }
-        }
-    createNumber()
-}  
-numbers()
-console.log(tabellone)   
+  };
+  createNumber();
+};
+numbers();
+console.log(tabellone);
 
+const pulsante = function () {
+  const but = document.querySelector("button");
+  but.addEventListener("click", () => {
+    const pressBot = Math.floor(Math.random() * 77);
+    console.log(pressBot);
+    const numeriLight = document.querySelectorAll(".numero");
+    const match = () => {
+      for (let i = 0; i < numeriLight.length; i++)
+        if (parseInt(numeriLight[i].innerText) === pressBot) {
+         numeriLight[i].classList.add("selected");
+      }
+  };
+  match();
+})};
 
-
-const pulsante = function(){
-    const but = document.querySelector('button')
-    but.addEventListener('click', ()=>{
-    const pressBot = Math.floor(Math.random()*77)
-    console.log(pressBot)
-    return
-    
-    })
-    const numeriLight = document.querySelectorAll('numero')
-    const match = ()=>{
-    for(let i=0; i < numeriLight.length;i++)
-    if(numeriLight[i].innerText === pressBot.innerText){
-        numeriLight[i].classList.add('selected')
-    }
-}
-match()
-    }
-        
-pulsante()
-
-
-
-
-
+pulsante();
